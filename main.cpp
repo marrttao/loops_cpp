fddb0259688413080a4e3b249dc791858fb0df67
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-20 19:39:28 +0200

    completed task 3

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-	return 0;
+    int range_start, range_end, sum = 0;
+    cout << "Enter the range start: ";
+    cin >> range_start;
+    cout << "Enter the range end: ";
+    cin >> range_end;
+
+    for (int i = range_start; i <= range_end; i++) {
+        sum += i;
+    }
+
+    cout << "The sum of numbers in the range is: " << sum << endl;
+    return 0;
 }
