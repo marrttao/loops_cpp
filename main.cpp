58e429a43956cd191e37c648374141f12d5adbeb
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-20 20:05:52 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
-	return 0;
+    int x, y;
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter y: ";
+    cin >> y;
+    int result = 1;
+    for (int i = 0; i < y; i++) {
+        result *= x;
+    }
+    cout << x << " to the power " << y << "is " << result << endl;
+    return 0;
 }
