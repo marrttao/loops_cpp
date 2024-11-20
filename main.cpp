684571f037362e1c579457fb5eb48e7541652b8c
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-20 19:55:51 +0200

    completed task 1

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-	return 0;
+    size_t sum = 0, a;
+    cout << "Enter a number: ";
+    cin >> a;
+
+    for (size_t i = a; i <= 500; i++) {
+        sum += i; 
+    }
+
+    cout << "The sum from " << a << " to 500 is: " << sum << endl;
+    return 0;
 }
