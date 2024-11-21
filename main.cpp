2efc93d8e4e5893c75e95ba2012e35302081c11e
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-21 15:27:33 +0200

    completed task 5

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main() {
+	int k;
+	cout << "Enter a k: " << endl;
+	cin >> k;
+	for (int i = 1; i <= 10; i++) {
+		cout << k << " * " << i << " = " << k*i << endl;
+	}
 	return 0;
 }
