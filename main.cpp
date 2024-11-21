c6ccaacd174743ee85fc4695acf5c058c99836f4
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-21 15:21:43 +0200

    completed task 4

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-	return 0;
+    int a;
+    size_t result = 1;
+    cout << "Enter a number: ";
+
+    do {
+        cin >> a;
+        if ((a < 0) || (a > 20)) { 
+            cout << "The number must be between 0 and 20. Please try again: ";
+        }
+    } while ((a < 0) || (a > 20));
+
+    for (int i = a; i <= 20; i++) {
+        result *= i;
+    }
+
+    cout << endl << "The product of numbers from " << a << " to 20 is: " << result << endl;
+    return 0;
 }
